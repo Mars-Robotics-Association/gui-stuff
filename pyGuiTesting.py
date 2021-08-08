19009d8c79a6010407019c3fa4627342c78a122e
isaacthoman <isaacthoman@outlook.com>
2021-08-07 17:21:17 -0700

    changing of URL

--- pyGuiTesting.py
+++ pyGuiTesting.py
@@ -1,44 +1,69 @@
 import dearpygui.dearpygui as dpg
-import dearpygui.themes as themes
-from dearpygui.logger import mvLogger
-from math import sin, cos
-import random
-import logging
 import threading
 import time
 import requests
 
 xVals = []
 yVals = []
-url = "https://api.isaacthoman.me/api/Gui" #This would be the URL of the robot's webserver
 
 series_id = dpg.generate_uuid()
+urlBox = dpg.generate_uuid()
+checkBox1 = dpg.generate_uuid()
+responseLabel = dpg.generate_uuid()
 
 xVal=0;
-scanning = 1
+scanning = 0
+keepGoing = 1
+
+def changeScanning():
+    global scanning
+    scanning = not scanning
+    if(scanning==True):
+        keepGoing=1
+        x = threading.Thread(target=bg_requests, args=(1,))
+        x.start()
+    else:
+        keepGoing=0
 
 
+requestGood = False
 def bg_requests(name):
-    while scanning==1:
-        resp = requests.get(url)
-        firstFix = ''.join(resp.text.split('"', 1))
-        secondFix = ''.join(firstFix.split('"', 1))
-        finalNumber = float(secondFix)
+    while keepGoing==1:
+        if scanning==1:
+            global requestGood
 
-        global xVal
-        global xVals
-        global yVals
-        xVal+=0.5
-        xVals.append(xVal)
-        yVals.append(finalNumber)
-        print(finalNumber)
+            requestGood = False
+            resp = requests.get(dpg.get_value(urlBox))
+            if(resp.status_code==200):
+                firstFix = ''.join(resp.text.split('"', 1))
+                secondFix = ''.join(firstFix.split('"', 1))
+                finalNumber = float(secondFix)
 
-       # dpg.set_item_label(test_id,'hey there')
-        dpg.set_value(series_id,[xVals,yVals])
-        time.sleep(0.1)
+                global xVal
+                global xVals
+                global yVals
+                xVal+=0.5
+                xVals.append(xVal)
+                yVals.append(finalNumber)
+                dpg.set_value(series_id,[xVals,yVals])
+                requestGood=True
 
+            else:
+                requestGood=False
+
+
+        time.sleep(0.2)
+        if (requestGood):
+            dpg.set_value(responseLabel, 'Connection Succeeded')
+            dpg.configure_item(responseLabel, color=(0, 255, 0))
+        else:
+            dpg.set_value(responseLabel, 'Connection Failed')
+            dpg.configure_item(responseLabel, color=(255, 0, 0))
 
 with dpg.window(label="Test", height=500, width=500):
+    dpg.add_checkbox(label="Collect Data", default_value=False,id=checkBox1,callback=changeScanning)
+    dpg.add_text("No Requests Yet",id=responseLabel)
+    dpg.add_input_text(label="Robot URL",id=urlBox,default_value="https://api.isaacthoman.me/api/Gui")
     with dpg.plot(label="Plot", height=-1, width=-1) as test_id:
         dpg.add_plot_axis(dpg.mvXAxis, label="x")
         dpg.add_plot_axis(dpg.mvYAxis, label="y")
@@ -47,9 +72,7 @@ with dpg.window(label="Test", height=500, width=500):
 
     dpg.plot
 
-x = threading.Thread(target=bg_requests, args=(1,))
-x.start()
 
-#dpg.setup_viewport()
+dpg.setup_viewport()
 dpg.start_dearpygui()
-scanning=0
+keepGoing=0
